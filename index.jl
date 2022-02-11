13e04fedfe2c751fadfca2d58188d89fb5621308
SabrinaJaye <sabrinajward@protonmail.com>
2022-02-11 05:41:43 +0000

    Deploying to gh-pages from  @ f72ea1d3997d9e897c008d352f02ce6890073e6c 🚀

---
+++ index.jl
@@ -0,0 +1,38 @@
+### A Pluto.jl notebook ###
+# v0.17.7
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ eacd3016-8afb-11ec-169d-4f9ee1f6fc24
+md"""
+# Sabby's Pluto Notebooks
+
+Welcome! You've found the draw I keep all my Pluto.jl notebooks - think
+code notebooks like jupyter, but specific to the Julia language, with live updating cells, interactive elements, and notebook specific package environments.
+
+Anyway, take a look around.
+Whenever I do any quick and dirty, or exploratory analyses I typically put it into one of these notebooks. But they're also great for writing nice, complete, interactive lessons or homeworks.
+
+[Example Notebook](My\ cool\ notebook)
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.1"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═eacd3016-8afb-11ec-169d-4f9ee1f6fc24
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
